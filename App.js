5374a5306de5ccaacef78f704f24310f018105f1
Pariwesh Gupta (C) <pariwesh@alchemysolutions.net>
2023-09-29 19:42:31 +0530

    first commit

--- App.js
+++ App.js
@@ -5,34 +5,39 @@ import Modal from 'react-native-modal';
 import { useState } from 'react';
 import Login from "./Login";
 import SMSComponent from './Sms';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import { NativeScreenNavigationContainer } from 'react-native-screens';
+const Stack = createNativeStackNavigator();
 export default function App() {
   const [visible, setVisible] = useState(false);
   function handlePress() {
     setVisible(!visible)
   }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <ScrollView onPress={value => alert(value)}>
-        <Text > Item 1</Text>
-        <Text> Item 1</Text>
-        <Text> Item 1</Text>
-      </ScrollView>
-      <Button title='Login' onPress={handlePress} />
-      <Modal isVisible={visible} >
-        <View>
-          <SMSComponent />
-
-          <View>
-            <Button title='Hide modal' onPress={handlePress}></Button>
-          </View>
-        </View>
-      </Modal>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='home' component={HomeScreen} options={{ title: 'Home' }}>
+        </Stack.Screen>
+        <Stack.Screen name='profile' component={ProfileScreen}>
+        </Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+const HomeScreen = ({navigation}) => {
+  return (<Button title='go back' onPress={() => {
+    console.log(navigation);
+    navigation.navigate('profile');
+  }}></Button>)
+}
 
+const ProfileScreen = ({ navigation, ruote }) => {
+  return <Text>this is profile screen.</Text>
+}
+const Stack1 = () => {
+
+}
 const styles = StyleSheet.create({
   input1: {
     width: '90%',
