3dfdb113a9ddaf0f230ff81863f07b277e6b8e99
Pariwesh Gupta (C) <pariwesh@alchemysolutions.net>
2023-10-04 19:41:26 +0530

    location feature

--- App.js
+++ App.js
@@ -5,12 +5,16 @@ import Modal from 'react-native-modal';
 import { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import Location1 from './Location';
+import * as Location from 'expo-location';
+import { useEffect } from 'react/cjs/react.development';
 const Drawer = createDrawerNavigator();
 export default function App() {
   const [visible, setVisible] = useState(false);
   function handlePress() {
     setVisible(!visible)
   }
+  
   return (
     <NavigationContainer>
       <Drawer.Navigator>
@@ -20,29 +24,38 @@ export default function App() {
         </Drawer.Screen>
       </Drawer.Navigator>
     </NavigationContainer>
-    // <View style={styles.container}>
-    //   <Text>Open up App.js to start working on your app!</Text>
-    //   <StatusBar style="auto" />
-    //   <ScrollView onPress={value => alert(value)}>
-    //     <Text > Item 1</Text>
-    //     <Text> Item 1</Text>
-    //     <Text> Item 1</Text>
-    //   </ScrollView>
-    //   <Button title='Login' onPress={handlePress} />
-    //   <Modal isVisible={visible} >
-    //     <View>
-    //       <SMSComponent />
-
-    //       <View>
-    //         <Button title='Hide modal' onPress={handlePress}></Button>
-    //       </View>
-    //     </View>
-    //   </Modal>
-    // </View>
+   
   );
 }
 const HomeScreen = ({navigation}) => {
-  return (<Button title='go back' onPress={() => navigation.navigate('profile')}></Button>)
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+  return (<View><Text>This is some text</Text>
+    {/* <Button title='go back1' onPress={() => navigation.navigate('profile')}>
+  </Button> */}
+  <Text style={styles.paragraph}>This is a location :{text}</Text>
+  </View>)
 
 }
 const ProfileScreen = ({ navigation, route }) => {
