b53ada9655e644f4ed740157c03a6f256dfc4888
Pariwesh Gupta (C) <pariwesh@alchemysolutions.net>
2023-10-06 20:23:51 +0530

    location feature

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, Alert } from 'react-native';
 import Modal from 'react-native-modal';
 import { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import * as Location from "expo-location";
+import { getLocaleDirection } from 'react-native-web/dist/cjs/modules/useLocale';
 const Drawer = createDrawerNavigator();
 export default function App() {
   const [visible, setVisible] = useState(false);
   function handlePress() {
     setVisible(!visible)
   }
+
   return (
     <NavigationContainer>
       <Drawer.Navigator>
@@ -20,29 +23,25 @@ export default function App() {
         </Drawer.Screen>
       </Drawer.Navigator>
     </NavigationContainer>
-    // <View style={styles.container}>
-    //   <Text>Open up App.js to start working on your app!</Text>
-    //   <StatusBar style="auto" />
-    //   <ScrollView onPress={value => alert(value)}>
-    //     <Text > Item 1</Text>
-    //     <Text> Item 1</Text>
-    //     <Text> Item 1</Text>
-    //   </ScrollView>
-    //   <Button title='Login' onPress={handlePress} />
-    //   <Modal isVisible={visible} >
-    //     <View>
-    //       <SMSComponent />
-
-    //       <View>
-    //         <Button title='Hide modal' onPress={handlePress}></Button>
-    //       </View>
-    //     </View>
-    //   </Modal>
-    // </View>
   );
 }
-const HomeScreen = ({navigation}) => {
-  return (<Button title='go back' onPress={() => navigation.navigate('profile')}></Button>)
+const HomeScreen = ({ navigation }) => {
+  
+  async function getLocation() {
+    alert("Error", 'something went wrong');
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    console.log(status);
+    if (status !== 'granted') {
+      alert("Error", 'something went wrong');
+      return;
+    }
+    let location = await Location.getCurrentPositionAsync({});
+    alert("result", location);
+  }
+  return (<View>
+    <Button title='go back' onPress={() => navigation.navigate('profile')}></Button>
+    <Button title='Get location' onPress={() => getLocation()}></Button>
+  </View>)
 
 }
 const ProfileScreen = ({ navigation, route }) => {
